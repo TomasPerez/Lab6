f2415940085f5c5e7fcdafbbf1f518015fcc2630
Tomas Perez <tperez48210@gmail.com>
2017-07-18 16:36:28 -0400

    first commit

---
+++ src/PiglatinLab.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by DELL on 7/14/2017.
+ */
+public class PiglatinLab {
+
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+        String userWord;
+        String pigTrans = scan.nextLine();
+
+
+        System.out.println("Hello Users! Welcome to Pig Latin Translater! ");
+        System.out.print("Please enter a word: ");
+        userWord = scan.nextLine().toLowerCase();
+
+        char i;
+        if {
+            ((i == 'a') || (i == 'e') || (i == 'i') || (i == 'o') || (i == 'u'))
+        }
+
+
+    }
+}
